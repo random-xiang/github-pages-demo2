29604da75c594bf346da1a400ee50d0b387352f2
Random <random.x@binance.com>
2022-04-08 18:40:50 +0800

    feat: update token

--- next.config.js
+++ next.config.js
@@ -16,4 +16,5 @@ module.exports = withBundleAnalyzer({
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
+  assetPrefix: '/github-pages-demo2/',
 });
